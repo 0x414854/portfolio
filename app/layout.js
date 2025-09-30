7412907c4c9b90bc213663a16cd8c1282f56dffd
0x414854 <0x414854@proton.me>
2025-09-30 12:28:39 +0200

    Add SpeedInsights

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import IonIconsLoader from "./lib/ionIconLoader";
 import Script from "next/script";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -80,6 +81,7 @@ export default function RootLayout({ children }) {
         </Script>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
